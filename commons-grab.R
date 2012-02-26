afabda6f98ad5be00f8bfdad71ca230675be8a90
Adam Hyland <protonk@gmail.com>
2012-02-26 06:03:08 -0600

    remove ns arg. vectorise some computations.

--- commons-grab.R
+++ commons-grab.R
@@ -2,7 +2,7 @@ library(rjson)
 
 # Enumerate categories from the Mediawiki API
 
-# Images on commons are in ns = 6 eventually I may just remove the arg
+# Images on commons are in ns = 6 
 
 # 3 subordinate functions and one main routine 
 # genCompCat, fetchfullURL both lead into
@@ -12,14 +12,14 @@ library(rjson)
 
 # Continue argument is added for categories w/ greater than 500 members
 
-genCompCat <- function(category, namespace) {
-  genCategoryURL <- function(category, namespace, continue = NULL) {
+genCompCat <- function(category) {
+  genCategoryURL <- function(category, continue = NULL) {
     url.start <-paste(
       "http://commons.wikimedia.org/w/api.php?", 
       "action=query",
       "list=categorymembers",
       paste("cmtitle", "=", "Category:", category, sep = ""),
-      paste("cmnamespace", "=", namespace, sep = ""),
+      "cmnamespace=6",
       "cmlimit=500",
       "cmtype=file",
       "cmprop=title",
@@ -28,10 +28,10 @@ genCompCat <- function(category, namespace) {
     if (is.null(continue)) url.gen <- url.start
     else url.gen <- paste(url.start, paste("cmcontinue", "=", continue, sep = ""), sep = "&")  
   }
-  cat.list.init <- fromJSON(file = genCategoryURL(category, namespace))
+  cat.list.init <- fromJSON(file = genCategoryURL(category))
   cat.unform <- unlist(cat.list.init$query$categorymembers)[names(unlist(cat.list.init$query$categorymembers)) == "title"]
   while (!is.null(cat.list.init$`query-continue`)) {
-    cat.list.init <- fromJSON(file = genCategoryURL(category, namespace, continue = unlist(cat.list.init$`query-continue`)[[1]]))
+    cat.list.init <- fromJSON(file = genCategoryURL(category, continue = unlist(cat.list.init$`query-continue`)[[1]]))
     cat.unform <- append(cat.unform, unlist(cat.list.init$query$categorymembers)[names(unlist(cat.list.init$query$categorymembers)) == "title"])
   }
   # API requires underscores instead of spaces
@@ -57,39 +57,34 @@ fetchfullURL <- function(baseurl, hiRES) {
     "format=json",
     sep = "&")
   if (hiRES) {
-    pageJSON <- fromJSON(file = gsub("|size", "", url.gen))
+    pageJSON <- fromJSON(file = sub("|size", "", url.gen))
     unname(unlist(pageJSON$query$pages)[grep("imageinfo.url", names(unlist(pageJSON$query$pages)))])
   } 
   else {
     pageJSON <- fromJSON(file = url.gen)
-    list(url = unname(unlist(pageJSON$query$pages)[grep("imageinfo.url", names(unlist(pageJSON$query$pages)))]), 
-         size = as.numeric(unname(unlist(pageJSON$query$pages)[grep("imageinfo.size", names(unlist(pageJSON$query$pages)))]))
+    cbind(unname(unlist(pageJSON$query$pages)[grep("imageinfo.url", names(unlist(pageJSON$query$pages)))]), 
+          unname(unlist(pageJSON$query$pages)[grep("imageinfo.size", names(unlist(pageJSON$query$pages)))])
          )
   }
 }
 
 # Default values set in this function only temporarily. 
 
-fetchCommonsCat <- function(category, namespace = 6, useragent, hiRES = FALSE) {
+fetchCommonsCat <- function(category, useragent, hiRES = FALSE) {
   # Mediawiki requires an informative user agent. Yours should be distinct
   options(HTTPUserAgent= useragent )
-  cat.final <- genCompCat(category, namespace)
-  resolved.URL <- character()
+  cat.final <- genCompCat(category)
   if (hiRES) {  
-    for (i in 1:length(cat.final)) {
-      resolved.URL[i] <- fetchfullURL(cat.final[i], hiRES = TRUE)
-    }
+      resolved.URL <- sapply(cat.final, fetchfullURL, hiRES = hiRES)
   }  
   else {
-    size.pic <- numeric()
-    for (i in 1:length(cat.final)) {
-      resolved.URL[i] <- fetchfullURL(cat.final[i], hiRES = FALSE)$url
-      size.pic[i] <- fetchfullURL(cat.final[i], hiRES = FALSE)$size
-    }
+    comb.res <- sapply(cat.final, fetchfullURL, hiRES = hiRES)
+    resolved.URL <- comb.res[1, ]
+    size.pic <- as.numeric(comb.res[2, ])
     resolved.URL <- resolved.URL[which(size.pic <= quantile(size.pic, 0.9))]
   }
   closeAllConnections()
-  return(resolved.URL)
+  unname(resolved.URL)
 }
 
 
